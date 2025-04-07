071150ced2478c75eb0ef6fd20f74b1e8fb1bc36
Fardeen NB - 🧑‍💻🧠 <56260586+fardeen0424@users.noreply.github.com>
2025-04-07 13:54:20 -0400

    Update

--- colab_demo.py
+++ colab_demo.py
@@ -18,11 +18,7 @@ def setup_promptsage():
         print("This script is designed to run in Google Colab.")
         return False
     
-    # Install required packages
-    pip install transformers datasets torch matplotlib seaborn wordcloud nltk
 
-    # Clone the repository (assuming it's on GitHub at this point)
-    git clone https://github.com/fardeen0424/PromptSage.git
     
     # Add to Python path
     promptsage_path = os.path.join(os.getcwd(), "promptsage")
